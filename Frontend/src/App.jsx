6c7385f5d7190975a30b8a26252fdde6e7b608ea
Abd-algabar <engabdalgabar@gmail.com>
2025-11-19 15:24:50 +0300

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import Nav from "./components/01-Nav/Nav.jsx";
 import { Routes, Route } from "react-router-dom";
 import Container from "@mui/material/Container";
-import Home from "./pages/Home.jsx";
-import Footer from "./components/07-Footer/Footer.jsx";
-import Login from "./pages/login/Login.jsx";
-import SignUp from "./pages/SignUp/SignUp.jsx";
-import AdoptCats from "./pages/Adopt cats/AdoptCats.jsx";
-import CatsForMating from "./pages/Cats for mating/CatsForMating.jsx";
-import CatPage from "./pages/cat details/catPage.jsx";
+
+
 
 import AppContent from "./AppContent.jsx";
 import { AuthProvider, useAuth } from './context/AuthContext.jsx';
