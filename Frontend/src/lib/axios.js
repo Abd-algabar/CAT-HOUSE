3343dcde22d5cb7d63aea148e42054da35113b71
Abd-algabar <engabdalgabar@gmail.com>
2025-11-19 13:06:57 +0300

    Update axios.js url

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const BASE_URL=import.meta.env.BASE_URL
 export const axiosInstance=axios.create(
     {
-        baseURL:"http://localhost:5000/api",
+        baseURL:"https://cat-house-backend.onrender.com/",
        
     }
 )
